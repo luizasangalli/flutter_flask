6e4bdbdafac1d9c626a753eb432ebcccd6822d7e
luiza-sangalli <luiza.sangalli@outlook.com>
2021-01-01 21:59:33 -0300

    qqe

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final GlobalKey<AnimatedListState> _listKey = GlobalKey();
   List<String> _data = [];
   static const String BOT_URL =
-      "https://superduperchat-api-heroku.herokuapp.com/"; // replace with server address
+      "https://superduperchat-api-heroku.herokuapp.com"; // replace with server address
   TextEditingController _queryController = TextEditingController();
 
   @override
